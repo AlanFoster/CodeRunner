d0d68363fcde686a269d3b6d892a9ab5bd6e7ead
Alan Foster <alan@alanfoster.me>
2014-06-08 01:12:27 +0100

    Integrate routing information, and maintain this routing state when a code example changes

--- backbone.js
+++ backbone.js
@@ -1,3 +1,4 @@
+
 var TutorialView = Backbone.View.extend({
     initialize: function() {
         var pages = this.$el.find(".page");
@@ -8,7 +9,7 @@ var TutorialView = Backbone.View.extend({
         this.animating = false;
     },
     render: function() {
-        this.$el.find(".page").eq(0).show();
+        this.$el.find(".page").eq(this.currentIndex).show();
         return this;
     },
     events: {
@@ -27,21 +28,23 @@ var TutorialView = Backbone.View.extend({
 
         var currentIndex = this.currentIndex;
         var newIndex = (currentIndex === 0)
-                ? this.pages.length - 1
-                : currentIndex - 1;
+            ? this.pages.length - 1
+            : currentIndex - 1;
         this.showPage(newIndex);
     },
     showPage: function(newIndex) {
         if(this.animating) return;
 
         this.animating = true;
+
         var oldIndex = this.currentIndex;
         this.currentIndex = newIndex;
+        // TODO - Perhaps we should be bound to a backbone model to get these events for free?
+        this.trigger("change:currentIndex", newIndex);
         var pages = this.$el.find(".page")
 
         var self = this;
         pages.eq(oldIndex).fadeOut(function(){
-            console.log("call back")
             pages.eq(newIndex).fadeIn(function() {
                 self.animating = false;
             });
@@ -52,4 +55,25 @@ var TutorialView = Backbone.View.extend({
 var tutorialView = new TutorialView({
     el: "#tutorial-box"
 });
+
+
+var CodeRouter = Backbone.Router.extend({
+    routes: {
+        "snippet/:id": "viewSnippet"
+    },
+    viewSnippet: function(id) {
+        tutorialView.showPage(id);
+    }
+});
+
+var codeRouter = new CodeRouter();
+Backbone.history.start();
+
+// Bind a listener to the carousel so that when it changes
+// we update the routing information
+tutorialView.on("change:currentIndex", function(newIndex){
+    codeRouter.navigate("snippet/" + newIndex);
+});
+
+// Render our views
 tutorialView.render();
